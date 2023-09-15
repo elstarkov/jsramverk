a6ed47c431bda4cd0721c76530e0c0ed859e2567
elstarkov <samuelward37@gmail.com>
2023-09-15 14:01:19 +0200

    Moved database test into test-routes

--- train/backend/test/test-routes.js
+++ train/backend/test/test-routes.js
@@ -1,6 +1,8 @@
 
 /* global it describe */
 
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
@@ -8,6 +10,20 @@ const server = require('../app.js');
 chai.should();
 
 chai.use(chaiHttp);
+
+const dsn = "mongodb://localhost:27017/trains";
+process.env.JSRAMVERK_DSN = dsn;
+
+const database = require("../db/mongo_database.js");
+
+before(async () => {
+    db = await database.getDb();
+});
+
+after(async () => {
+    await db.collection.deleteMany();
+    await db.client.close();
+});
 
 describe('app', () => {
     describe('GET /', () => {
@@ -64,9 +80,28 @@ describe('app', () => {
                         res.should.have.status(200);
                         res.body.should.be.an("object");
                         res.body.data.should.be.an("array");
-                        res.body.data.length.should.be.above(0);
                         done();
                     });
+            });
+        });
+    });
+
+    describe('app', () => {
+        describe('POST /tickets', () => {
+            it('should create a new ticket', async () => {
+                const doc = {
+                    code: "1337",
+                    trainnumber: "TEST_TRAIN",
+                    traindate: "24/1337",
+                };
+
+                const res = await chai.request(server)
+                    .post('/tickets')
+                    .send(doc);
+
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+
             });
         });
     });
