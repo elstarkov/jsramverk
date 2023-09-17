0735826c87e2a10b3ad0f37c1bdbf9f11ae0f8e3
Yli <jeyl22@bth.se>
2023-09-17 18:44:06 +0200

    Small fixes

--- train/frontend/src/api.js
+++ train/frontend/src/api.js
@@ -1,33 +1,36 @@
 const port = 6060;
 
-async function getTickets(){
-    const resp = await fetch(`http://localhost:${port}/tickets`);
-    const res = await resp.json();
-    return res.data;
-}
+const api = {
+    getTickets: async function (){
+        const resp = await fetch(`http://localhost:${port}/tickets`);
+        const res = await resp.json();
+        return res.data;
+    },
+    
+    getCodes: async function (){
+        const resp = await fetch(`http://localhost:${port}/codes`);
+        const res = await resp.json();
+        return res.data;
+    },
+    
+    getDelayed: async function (){
+        const resp = await fetch(`http://localhost:${port}/delayed`);
+        const res = await resp.json();
+        return res.data;
+    },
+    
+    createTicket: async function (newTicketObj){
+        const response = await fetch(`http://localhost:${port}/tickets`, {
+            body: JSON.stringify(newTicketObj),
+            headers: {
+                'content-type': 'application/json'
+            },
+            method: 'POST'
+        })
+    
+        return response
+    },
+};
 
-async function getCodes(){
-    const resp = await fetch(`http://localhost:${port}/codes`);
-    const res = await resp.json();
-    return res.data;
-}
 
-async function getDelayed(){
-    const resp = await fetch(`http://localhost:${port}/delayed`);
-    const res = await resp.json();
-    return res.data;
-}
-
-async function createTicket(newTicketObj){
-    const response = await fetch(`http://localhost:${port}/tickets`, {
-        body: JSON.stringify(newTicketObj),
-        headers: {
-            'content-type': 'application/json'
-        },
-        method: 'POST'
-    })
-
-    return response
-}
-
-export default {getTickets, getCodes, getDelayed, createTicket};
+export default api;
