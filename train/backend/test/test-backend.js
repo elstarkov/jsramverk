c9db29fb3c8554826e25d0d762372db2f6838ab1
elstarkov <samuelward37@gmail.com>
2023-09-19 17:17:23 +0200

    Trying again...

--- train/backend/test/test-backend.js
+++ train/backend/test/test-backend.js
@@ -17,7 +17,7 @@ process.env.JSRAMVERK_DSN = dsn;
 
 const socketIO = require('socket.io-client');
 
-const port = 8080;
+process.env.TEST_PORT = 3000;
 
 const database = require("../db/mongo_database.js");
 const fetchTrainPositions = require("../models/trains.js");
@@ -115,8 +115,8 @@ describe('app', () => {
             let isDone = false;
 
             before(function(done) {
-                server.listen(port, () => {
-                    console.log(`Test server listening on port ${port}`);
+                server.listen(process.env.TEST_PORT, () => {
+                    console.log(`Test server listening on port ${process.env.TEST_PORT}`);
                     done();
                 });
             });
@@ -129,7 +129,7 @@ describe('app', () => {
             });
 
             beforeEach(function() {
-                client = socketIO(`http://127.0.0.1:${port}`, {
+                client = socketIO(`http://127.0.0.1:${process.env.TEST_PORT}`, {
                     transports: ['websocket'],
                     forceNew: true,
                 });
